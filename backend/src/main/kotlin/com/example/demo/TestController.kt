a7fbd288465ec1b87760527b8ef431b1503d7fd5
h3nryc0ding <hr.richterhenry@gmail.com>
2023-12-24 00:49:13 +0100

    Changed controller to return paramMap for debugging and testing.

--- backend/src/main/kotlin/com/example/demo/TestController.kt
+++ backend/src/main/kotlin/com/example/demo/TestController.kt
@@ -2,11 +2,12 @@ package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class HelloController {
+class TestController {
     @GetMapping("/")
-    fun index(): String {
-        return "Greetings from Spring Boot!"
+    fun index(@RequestParam params: Map<String, String>): Map<String, String> {
+        return params
     }
 }
