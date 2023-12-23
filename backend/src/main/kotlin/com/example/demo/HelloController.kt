47ed559ecd744f44a88beb2e1298ca2a622311db
h3nryc0ding <hr.richterhenry@gmail.com>
2023-12-24 00:37:48 +0100

    Added profile based CORS.

--- backend/src/main/kotlin/com/example/demo/HelloController.kt
+++ backend/src/main/kotlin/com/example/demo/HelloController.kt
@@ -1,12 +1,9 @@
 package com.example.demo
 
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
-@CrossOrigin(origins = ["http://130.61.237.219.nip.io"], maxAge = 3600)
 class HelloController {
     @GetMapping("/")
     fun index(): String {
---
+++ backend/src/main/kotlin/com/example/demo/config/CorsConfig.kt
@@ -0,0 +1,44 @@
+package com.example.demo.config
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+
+@Configuration
+class CorsConfig {
+    @Bean
+    @Profile("dev")
+    fun corsFilterDev(): FilterRegistrationBean<*> {
+        return corsFilter("http://localhost")
+    }
+
+    @Bean
+    @Profile("ci")
+    fun corsFilterCI(): FilterRegistrationBean<*> {
+        return corsFilter("http://localhost")
+    }
+
+    @Bean
+    @Profile("prod")
+    fun corsFilterProd(): FilterRegistrationBean<*> {
+        return corsFilter("http://130.61.237.219.nip.io")
+    }
+
+    private fun corsFilter(url: String): FilterRegistrationBean<*> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin(url)
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        return FilterRegistrationBean(CorsFilter(source)).apply {
+            this.order = 0
+        }
+    }
+}
