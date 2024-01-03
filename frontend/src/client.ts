3cc4d82bbd2c6b829e89cbf6b84ec528e147103a
h3nryc0ding <hr.richterhenry@gmail.com>
2024-01-03 19:27:50 +0000

    Added lets encrypt certificate.

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -4,7 +4,7 @@ import { browser, dev } from '$app/environment';
 
 const graphqlUrl = () => {
 	if (dev) return 'http://localhost:8080/graphql';
-	if (browser) return 'http://api.130.61.237.219.nip.io/graphql';
+	if (browser) return 'https://api.130.61.237.219.nip.io/graphql';
 	return 'http://backend:80/graphql';
 };
 const subscriptionUrl = () => {
