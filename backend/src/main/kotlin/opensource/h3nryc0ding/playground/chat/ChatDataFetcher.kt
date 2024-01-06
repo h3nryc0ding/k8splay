6a2a162c23e805fa96a551a194e73dd79d727b2c
h3nryc0ding <hr.richterhenry@gmail.com>
2024-01-06 15:34:19 +0000

    Linted after package rename.

--- backend/src/main/kotlin/opensource/h3nryc0ding/playground/chat/ChatDataFetcher.kt
+++ backend/src/main/kotlin/opensource/h3nryc0ding/playground/chat/ChatDataFetcher.kt
@@ -2,10 +2,10 @@
 
 package opensource.h3nryc0ding.playground.chat
 
-import opensource.h3nryc0ding.playground.generated.types.MessageInput
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsData
 import com.netflix.graphql.dgs.InputArgument
+import opensource.h3nryc0ding.playground.generated.types.MessageInput
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.event.EventListener
 import org.springframework.data.annotation.Id
@@ -19,19 +19,19 @@ import java.util.UUID
 
 @RedisHash("message")
 data class Message(
-        @Id
-        val id: UUID = UUID.randomUUID(),
-        val text: String,
-        val creator: String,
-        val timestamp: String,
+    @Id
+    val id: UUID = UUID.randomUUID(),
+    val text: String,
+    val creator: String,
+    val timestamp: String,
 ) {
     fun toDTO() =
-            opensource.h3nryc0ding.playground.generated.types.Message(
-                    id = { this.id.toString() },
-                    text = { this.text },
-                    creator = { this.creator },
-                    timestamp = { this.timestamp },
-            )
+        opensource.h3nryc0ding.playground.generated.types.Message(
+            id = { this.id.toString() },
+            text = { this.text },
+            creator = { this.creator },
+            timestamp = { this.timestamp },
+        )
 }
 
 interface MessageRepository : CrudRepository<Message, Long>
@@ -47,8 +47,8 @@ class MessageSentPublisher(private val applicationEventPublisher: ApplicationEve
 
 @DgsComponent
 class MessageDataFetcher(
-        private val messageRepository: MessageRepository,
-        private val messageSentPublisher: MessageSentPublisher,
+    private val messageRepository: MessageRepository,
+    private val messageSentPublisher: MessageSentPublisher,
 ) {
     private val processor = DirectProcessor.create<Message>().serialize()
     private val flux = processor.sink()
@@ -65,14 +65,14 @@ class MessageDataFetcher(
 
     @DgsData(parentType = "Mutation", field = "messageSend")
     fun sendMessage(
-            @InputArgument input: MessageInput,
+        @InputArgument input: MessageInput,
     ): opensource.h3nryc0ding.playground.generated.types.Message {
         val message =
-                Message(
-                        text = input.text,
-                        creator = input.creator,
-                        timestamp = LocalDateTime.now().toString(),
-                )
+            Message(
+                text = input.text,
+                creator = input.creator,
+                timestamp = LocalDateTime.now().toString(),
+            )
         return messageRepository.save(message).let {
             messageSentPublisher.publish(it)
             it.toDTO()
