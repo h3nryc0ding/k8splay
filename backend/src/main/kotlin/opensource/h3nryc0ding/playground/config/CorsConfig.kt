5a654307685375cbfed94c2e24aa64dd60c444d4
h3nryc0ding <hr.richterhenry@gmail.com>
2024-01-20 23:09:09 +0000

    refactor: move to custom domain

--- backend/src/main/kotlin/opensource/h3nryc0ding/playground/config/CorsConfig.kt
+++ backend/src/main/kotlin/opensource/h3nryc0ding/playground/config/CorsConfig.kt
@@ -1,40 +1,26 @@
 package opensource.h3nryc0ding.playground.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 
 @Configuration
 class CorsConfig {
-    @Bean
-    @Profile("dev")
-    fun corsFilterDev(): FilterRegistrationBean<*> {
-        return corsFilter(arrayOf("http://localhost:5173"))
-    }
+    @Value("\${FRONTEND_URL}")
+    lateinit var baseDomain: String
 
     @Bean
-    @Profile("ci")
-    fun corsFilterCI(): FilterRegistrationBean<*> {
-        return corsFilter(arrayOf("http://localhost"))
-    }
-
-    @Bean
-    @Profile("prod")
-    fun corsFilterProd(): FilterRegistrationBean<*> {
-        return corsFilter(arrayOf("https://130.61.237.219.nip.io"))
-    }
-
-    private fun corsFilter(urls: Array<String>): FilterRegistrationBean<*> {
+    fun corsFilter(): FilterRegistrationBean<*> {
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
         config.allowCredentials = true
-        for (url in urls) {
-            config.addAllowedOrigin(url)
-        }
+
+        config.addAllowedOrigin(baseDomain)
+
         config.addAllowedHeader("*")
         config.addAllowedMethod("*")
         source.registerCorsConfiguration("/**", config)
--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,15 +1,16 @@
 import { HoudiniClient, subscription } from '$houdini';
 import { createClient } from 'graphql-ws';
 import { browser, dev } from '$app/environment';
+import { env } from '$env/dynamic/public';
 
 const graphqlUrl = () => {
 	if (dev) return 'http://localhost:8080/graphql';
-	if (browser) return 'https://api.130.61.237.219.nip.io/graphql';
+	if (browser) return `https://api.${env.PUBLIC_BACKEND_HOST}/graphql`;
 	return 'http://backend:80/graphql';
 };
 const subscriptionUrl = () => {
 	if (dev) return 'ws://localhost:8080/subscriptions';
-	return 'wss://api.130.61.237.219.nip.io/subscriptions';
+	return `wss://api.${env.PUBLIC_BACKEND_HOST}/subscriptions`;
 };
 
 export default new HoudiniClient({
