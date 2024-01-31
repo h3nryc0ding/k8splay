1df2552d309613662b3c2b6dd46ebbe08cff1e83
h3nryc0ding <hr.richterhenry@gmail.com>
2024-01-31 11:41:54 +0000

    chore: cleanup

--- backend/src/test/kotlin/opensource/h3nryc0ding/playground/BackendApplicationTest.kt
+++ backend/src/test/kotlin/opensource/h3nryc0ding/playground/BackendApplicationTest.kt
@@ -1,9 +1,11 @@
 package opensource.h3nryc0ding.playground
 
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.ApplicationContext
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -18,7 +20,8 @@ class BackendApplicationTest {
     }
 
     @Test
-    fun contextLoads() {
+    fun contextLoads(applicationContext: ApplicationContext?) {
+        assertNotNull(applicationContext)
     }
 
     @Test
