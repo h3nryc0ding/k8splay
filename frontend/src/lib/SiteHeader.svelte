f43bcc7bf6197b8494e87379ce5889d2463e5e7e
h3nryc0ding <hr.richterhenry@gmail.com>
2024-03-01 18:36:07 +0000

    chore: format

--- frontend/src/lib/SiteHeader.svelte
+++ frontend/src/lib/SiteHeader.svelte
@@ -37,7 +37,10 @@
 					/>
 					<span class="sr-only">Toggle theme</span>
 				</Button>
-				<Button href="`https://${env.PUBLIC_BACKEND_DOMAIN}/oauth2/authorization/keycloak`" variant="ghost">
+				<Button
+					href="`https://${env.PUBLIC_BACKEND_DOMAIN}/oauth2/authorization/keycloak`"
+					variant="ghost"
+				>
 					<KeyRound class="mr-2 h-5 w-5" />
 					Login
 				</Button>
