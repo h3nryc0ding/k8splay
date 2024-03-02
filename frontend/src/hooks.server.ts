457ddaee66e9b07bd061efd5d2e2ebb5d13a30ff
h3nryc0ding <hr.richterhenry@gmail.com>
2024-03-02 18:10:22 +0000

    chore: disable console logs

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,8 +3,5 @@ import type { HandleFetch } from '@sveltejs/kit';
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	const cookie = event.request.headers.get('cookie');
 	if (cookie) request.headers.set('cookie', cookie);
-	console.log('cookie', JSON.stringify(cookie, null, 2));
-	console.log('event', JSON.stringify(event, null, 2));
-	console.log('request', JSON.stringify(request, null, 2));
 	return await fetch(request);
 };
