170e60f53f597c7443dc05cb71e19ea41a379e3d
h3nryc0ding <101796272+h3nryc0ding@users.noreply.github.com>
2024-03-06 16:05:21 +0100

    feat: update readme and default page (#121)
    * chore: update readme link to api
    * feat: update landing page
    * fix: test

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,51 +1,53 @@
-<article class="container prose pt-4 dark:prose-invert">
-	<h1>Welcome</h1>
-	<h2>What is this website about?</h2>
+<article class="container prose my-8 max-w-4xl dark:prose-invert">
+	<h1>MicroK8s Playground</h1>
+	<h2>Motivation</h2>
 	<p>
-		This website is a place where I test out new concepts and technologies that interest me. While I
-		don&#39;t focus on the visual or responsive part of the website, I do aim to learn as much as
-		possible from each project.
+		This repository is a playground for me to experiment with Kubernetes and its ecosystem. It is a
+		full stack application with a Spring Boot backend, a SvelteKit frontend, and a Keycloak server
+		for authentication.
 	</p>
-	<h2>What is the technology stack?</h2>
-	<p>Here are the base frameworks used for this:</p>
+	<h2>Features</h2>
 	<ul>
-		<li>Backend: Spring Boot</li>
-		<li>API: GraphQL</li>
-		<li>Frontend: Sveltekit</li>
+		<li>GraphQL API</li>
+		<li>SSO Authentication</li>
+		<li>SSL Encryption using Let&#39;s Encrypt and cert-manager</li>
+		<li>WebSocket based live chat</li>
 	</ul>
-	<p>In the lower section, you will find the packages or dependencies used.</p>
-	<h2>What&#39;s working?</h2>
+	<h3>Backend</h3>
+	<p>Deployed at <a href="https://api.k8splay.xyz/graphiql">api.k8splay.xyz</a></p>
+	<ul>
+		<li>Reactive Spring Boot with WebFlux and Kotlin</li>
+		<li>Persistence with R2DBC and MongoDB</li>
+		<li>Authentication and Authorization using Keycloak and oAuth2</li>
+		<li>GraphiQL available at <code>/graphiql</code></li>
+	</ul>
+	<h3>Frontend</h3>
+	<p>Deployed at <a href="https://k8splay.xyz">k8splay.xyz</a></p>
+	<ul>
+		<li>SvelteKit</li>
+		<li>ShadCN and TailwindCSS for styling</li>
+		<li>Houdini GraphQL client for SSR</li>
+		<li>Authenticated routes</li>
+		<li>All pages support SSR with in-cluster backend requests</li>
+	</ul>
+	<h3>Keycloak</h3>
+	<p>Deployed at <a href="https://accounts.k8splay.xyz">accounts.k8splay.xyz</a></p>
+	<ul>
+		<li>Optimized Docker image</li>
+		<li>Persistence with PostgreSQL</li>
+		<li>Custom theme</li>
+	</ul>
+	<h3>DevOps</h3>
 	<p>
-		Currently, this machine is running in the Oracle Cloud on an always-free VM. I set up a microk8s
-		cluster which features:
+		Deployed at <code>&lt;PR&gt;.test.k8splay.xyz</code>. Please replace <code>&lt;PR&gt;</code>
+		with the PR number, e.g. <code>pr-1.test.k8splay.xyz</code>. The API is deployed at
+		<code>api.&lt;PR&gt;.test.k8splay.xyz</code>.
+		<br />The deployment is automatically deleted when the PR is closed.
 	</p>
+	<p>Implemented using GitHub Actions:</p>
 	<ul>
-		<li>SSR rendering with direct connection via service discovery</li>
-		<li>Redis for temporary data</li>
-	</ul>
-	<p>
-		As full-stack features, there is currently only the live chat. There is definitely more to come.
-	</p>
-	<h2>What&#39;s coming?</h2>
-	<p>There is a lot of stuff planned for this.</p>
-	<p>Currently, the focus is set to:</p>
-	<ul>
-		<li>OAuth 2 via Spring</li>
-	</ul>
-	<h2>Dependencies</h2>
-	<ul>
-		<li>
-			Backend
-			<ul>
-				<li>Netflix DGS</li>
-			</ul>
-		</li>
-		<li>
-			Frontend
-			<ul>
-				<li>Houdini GraphQL</li>
-				<li>Shadcn Svelte</li>
-			</ul>
-		</li>
+		<li>Automated prod deployment</li>
+		<li>Automated test deployments on pull requests</li>
+		<li>Automated linting and testing</li>
 	</ul>
 </article>
